6ce3df73b87d099d86b82b32865e9aec2de62b23
gingerbreadmocha <jennyzhou0302@gmail.com>
2017-07-17 19:13:02 -0700

    Shop category migration fixed.

--- db/migrate/20170601043801_remove_category_from_shop_item.rb
+++ db/migrate/20170601043801_remove_category_from_shop_item.rb
@@ -1,6 +1,6 @@
 class RemoveCategoryFromShopItem < ActiveRecord::Migration[5.1]
   def change
-    remove_reference :shop_items, :category, foreign_key: true
+    remove_reference :shop_items, :shop_category, foreign_key: true
     add_reference :shop_items, :shop_category, foreign_key: true
   end
 end
